d19bb8567bf345da6e23c03f59033ca8ca9e2b63
cam-emmauscollege <45658387+cam-emmauscollege@users.noreply.github.com>
2025-09-09 23:48:04 +0200

    changed scripts

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,13 +1,17 @@
-# executed once after container had been build, before it is started
+#!/bin/bash
+# executed once after container has been built, before it is started
+
+set -e
 
 # install packages
 sudo apt-get update
-yes y | sudo apt install wmctrl
+yes y | sudo apt-get install wmctrl
 
 # download and install filius
 cd /tmp
-curl -o filius.deb https://www.lernsoftware-filius.de/downloads/Setup/filius_latest_all.deb
-sudo dpkg -i filius.deb
+curl -L -o filius.deb https://www.lernsoftware-filius.de/downloads/Setup/filius_latest_all.deb
+sudo dpkg -i filius.deb || sudo apt-get -f install -y
 
-# configure novnc
-sudo cp -f /workspaces/filius/.devcontainer/noVNC-1.2.0_index.html /usr/local/novnc/noVNC-1.2.0/index.html
+# configure novnc (relative to workspace root)
+WORKSPACE_DIR="$(pwd)"
+sudo cp -f "$WORKSPACE_DIR/.devcontainer/noVNC-1.2.0_index.html" /usr/local/novnc/noVNC-1.2.0/index.html
